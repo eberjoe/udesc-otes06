463a4da89162d367e4cb491b17c9c45da3ac2b38
eberjoe <eberjoe@gmail.com>
2021-08-24 01:17:45 -0300

    Adjust environment variable

--- mobile/src/services/neo.js
+++ mobile/src/services/neo.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { NASA_API_KEY } from '@env';
 
 const neo = axios.create({
   baseURL: 'https://api.nasa.gov/neo/rest/v1',
   params: {
-    api_key: process.env.NASA_API_KEY
+    api_key: NASA_API_KEY
   }
 });
 
--- mobile/src/views/Asteroids.js
+++ mobile/src/views/Asteroids.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback, useState, useRef, useEffect } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { pt } from 'date-fns/locale';
+import { formatRelative, isBefore } from 'date-fns';
+import neo from '../services/neo';
 
 const Asteroids = ({ navigation }) => {
   const user = navigation.getParam('user');
 
+  const mountedRef = useRef(true);
+  const [asteroids, setAsteroids] = useState([]);
+
+  const load = useCallback(async () => {
+    if (mountedRef.current) {
+      const batch = await (await neo.get('/feed')).data.near_earth_objects;
+      const now = new Date();
+      const tempAsteroids = [];
+      for (const day in batch) {
+        tempAsteroids.push(
+          ...batch[day].filter(asteroid => isBefore(
+            now, new Date(
+              asteroid.close_approach_data[0].epoch_date_close_approach
+            )
+          )).map(asteroid => ({
+            id: asteroid.id,
+            name: asteroid.name,
+            passTimeFormatted: formatRelative(
+              new Date(
+                asteroid.close_approach_data[0].epoch_date_close_approach
+              ), now, { locale: pt }
+            ),
+            passTime: asteroid.close_approach_data[0].epoch_date_close_approach,
+            passDistance: `${Intl.NumberFormat('pt').format(
+              asteroid.close_approach_data[0].miss_distance.kilometers
+            )} km`,
+            estDiameter: (asteroid.estimated_diameter.kilometers.estimated_diameter_min + asteroid.estimated_diameter.kilometers.estimated_diameter_max) / 2,
+            isDangerous: asteroid.is_potentially_hazardous_asteroid
+          }))
+        );
+      }
+      tempAsteroids.sort((a, b) => a.passTime - b.passTime);
+      setAsteroids(tempAsteroids);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (mountedRef.current) {
+      load();
+    }
+    return () => {
+      mountedRef.current = false;
+    }
+  }, [load]);
+
+  const choose = (id) => {
+    console.log(id);
+  }
+
   return (
     <View>
       <Text>{`${user} Chegou!`}</Text>
+      {asteroids.map((asteroid, i) => (
+        <TouchableOpacity onPress={() => choose(asteroid.id)} key={i}>
+          <Text>{asteroid.name}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
